c83933c76a9dd7d443de5eeb6807162223bbbc7c
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    breeze: Drop Mediaextractor Seccomp Policy
    - These syscalls are now allowed by default.
    
    Change-Id: Id5e74954e8fed0d6c993a9b44bc7e56b67450491
    Signed-off-by: Debayan <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -135,8 +135,7 @@ PRODUCT_PACKAGES += \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \

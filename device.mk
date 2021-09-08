49ebdf96b4ceff1a559b38bb0b1f12df1305b696
Debayan Kar <kardebayan3@gmail.com>
2021-09-08 10:02:06 +0530

    breeze: Patchelf hostapd to load VNDK 28 version of libhwbinder.so
    * Fixes :-
    
    - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/hostapd": cannot locate symbol "_ZN7android8hardware14IPCThreadState27disableBackgroundSchedulingEb" referenced by "/vendor/bin/hw/hostapd"...
    
    Change-Id: I5d5f536107e074f506ddb7b428f50def857c7fc8
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -201,8 +201,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-sp/libcompiler_rt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcompiler_rt.so \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib/libc++-v28.so \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-sp/libhwbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhwbinder-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcompiler_rt.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcompiler_rt.so \
-    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++-v28.so
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++-v28.so \
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhwbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhwbinder-v28.so
 
 # WiFi
 PRODUCT_PACKAGES += \

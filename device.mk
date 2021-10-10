e76a8c5c8ce17becc23a904984ca9975f808d904
Debayan Kar <kardebayan3@gmail.com>
2021-10-10 19:33:32 +0530

    breeze: Build vendor variant of android.hardware.bluetooth.a2dp@1.0
    * Fixes :-
    
    - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.audio@4.0-service-mediatek": library "android.hardware.bluetooth.a2dp@1.0.so" not found: needed by main executable
    
    Change-Id: Ic82ea875d55f7e7b2613fdca06ea29e03db811a3

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0.vendor
 
 # Camera

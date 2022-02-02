227eeea8d660a70d25154918194204ef584859df
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-02 22:17:46 +0530

    breeze: Build libstdc++ from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I537b7362e53ca7f72bc5be40645235fb60417311

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4.vendor \
     android.hardware.camera.provider@2.4.vendor
 
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     libvulkan

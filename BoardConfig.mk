8a6649c30d5c5c03ba1c61a3a4eb03ca13283930
Michael Bestas <mkbestas@lineageos.org>
2022-12-27 21:31:40 +0530

    breeze: sepolicy: Use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS Fixes: warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead. warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead. warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,7 @@ VENDOR_SECURITY_PATCH := 2021-04-05
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK

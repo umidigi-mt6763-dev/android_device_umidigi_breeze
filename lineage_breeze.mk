3134aca1111aefc9dbc74ca086e38e806fda3072
Hadenix <vsegda_online_007@bk.ru>
2023-10-08 14:56:32 +0300

    breeze: Initial skeleton
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ lineage_breeze.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from A5_Pro device
+$(call inherit-product, device/umidigi/breeze/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := breeze
+PRODUCT_NAME := lineage_breeze
+PRODUCT_BRAND := UMIDIGI
+PRODUCT_MANUFACTURER := UMIDIGI
+
+PRODUCT_GMS_CLIENTID_BASE := android-agold

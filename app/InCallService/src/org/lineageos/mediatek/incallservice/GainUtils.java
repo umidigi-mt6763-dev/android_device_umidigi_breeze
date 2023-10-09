AndroidHQ254/android_device_infinix_x610_h651

breeze: InCallService: Bugfixes and refactor * Refactor and cleanup code * Use proper way of getting audioDevice with getPort().type() * Stop mixing up values between AudioSystem and AudioManager,   as values between AudioManager (java) and AudioSystem (native)   could change and become different from eachother.
Signed-off-by: bengris32 <bengris32@protonmail.ch>

InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
@@ -1,0 +1,16 @@
+package org.lineageos.mediatek.incallservice;
+
+import android.media.AudioSystem;
+
+import android.util.Log;
+
+public class GainUtils {
+    public static final String LOG_TAG = "MtkInCallService";
+
+    public static void setGainLevel(int audioDevice, int gainIndex, int streamType) {
+        String parameters = String.format("volumeDevice=%d;volumeIndex=%d;volumeStreamType=%d",
+                                          audioDevice, Math.min(7, gainIndex), streamType);
+        Log.d(LOG_TAG, "Setting audio parameters to: " + parameters);
+        AudioSystem.setParameters(parameters);
+    }
+}

umidigi-mt6763-dev/android_device_umidigi_breeze

Change-Id: I2311ed02b88148b71b374b19aaaf458f82f23824

app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
@@ -1,0 +1,16 @@
+package org.lineageos.mediatek.incallservice;
+
+import android.media.AudioSystem;
+
+import android.util.Log;
+
+public class GainUtils {
+    public static final String LOG_TAG = "MediatekInCallService";
+
+    public static void setGainLevel(int audioDevice, int gainIndex, int streamType) {
+        String parameters = String.format("volumeDevice=%d;volumeIndex=%d;volumeStreamType=%d",
+                                          audioDevice, Math.min(7, gainIndex), streamType);
+        Log.d(LOG_TAG, "Setting audio parameters to: " + parameters);
+        AudioSystem.setParameters(parameters);
+    }
+}